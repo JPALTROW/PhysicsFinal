ef90792236f6a76714727ce99f047f4e16c9ab46
Josiah-Moltz <jmoltz30@stuy.edu>
2023-05-22 11:01:41 -0400

    equilibrium?

--- take1.py
+++ take1.py
@@ -3,7 +3,8 @@ import random
 n=3
 
 MASS = 1
-LENGTH = 1
+DISTANCE = 2
+EQUILIBRIUM = 1
 G = 9.81
 k = 1
 
@@ -16,9 +17,9 @@ dt = .001
 
 def init():
     for e in range(n):
-        boxes.append(box(pos = vector(6,10-e*LENGTH-LENGTH/2,0), size = vector(LENGTH,1,1), color = vector(random.random(), random.random(), random.random())))
+        boxes.append(box(pos = vector(6,10-e*DISTANCE-DISTANCE/2,0), size = vector(DISTANCE,1,1), color = vector(random.random(), random.random(), random.random())))
         velocities.append(vector(0,0,0))
-        forces.append(vector(0, -MASS*G, 0))
+        forces.append(vector(0, 0, 0))
 
 def move():
     for e in range(n):
@@ -28,9 +29,10 @@ def move():
 
 def update_forces():
     for i in range(n):
-        forces[i] = vector(0, -MASS*G, 0)
+        forces[i] = vector(0, 0, 0)
     for i in range(n-1):
         diff = boxes[i+1].pos - boxes[i].pos
+        diff = (diff.mag - EQUILIBRIUM) / diff.mag * diff
         forces[i] += diff*k
         forces[i+1] += diff*(-k)
 
